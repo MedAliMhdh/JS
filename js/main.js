56332373d2620f70eb57d80917eaad9d4f53920f
mahdhaoui med ali <med_ali_mahdhaoui@yahoo.fr>
2020-11-13 23:52:24 +0100

    sum workin on all buttons

--- js/main.js
+++ js/main.js
@@ -6,9 +6,13 @@ function addItem(x) {
   } else {
     alert("stock epuisé");
   }
-  var itemsNumber = document.getElementById("input1").getAttribute("value"); //.value;
+  var itemsNumber1 = document.getElementById("input1").getAttribute("value"); //.value;
+  var itemsNumber2 = document.getElementById("input2").getAttribute("value"); //.value;
+  var itemsNumber3 = document.getElementById("input3").getAttribute("value"); //.value;
   document.getElementById("somme").innerHTML =
-    itemsNumber * parseInt(prix1, 10);
+    itemsNumber1 * parseInt(prix1, 10) +
+    itemsNumber2 * parseInt(prix2, 10) +
+    itemsNumber3 * parseInt(prix3, 10);
 }
 
 function susbstructItem(y) {
@@ -17,34 +21,14 @@ function susbstructItem(y) {
     y.setAttribute("value", +y.value - 1);
     // y.value = +y.value - 1;
   }
-  var itemsNumber = document.getElementById("input1").getAttribute("value"); //.value;
+  var itemsNumber1 = document.getElementById("input1").getAttribute("value"); //.value;
+  var itemsNumber2 = document.getElementById("input2").getAttribute("value"); //.value;
+  var itemsNumber3 = document.getElementById("input3").getAttribute("value"); //.value;
   document.getElementById("somme").innerHTML =
-    itemsNumber * parseInt(prix1, 10);
+    itemsNumber1 * parseInt(prix1, 10) +
+    itemsNumber2 * parseInt(prix2, 10) +
+    itemsNumber3 * parseInt(prix3, 10);
 }
-
-// function addItem(x) {
-//   x = document.getElementById(x);
-//   if (x.value < 6) {
-//     x.setAttribute("value", +x.value + 1);
-//     // x.value = +x.value + 1;
-//   } else {
-//     alert("stock epuisé");
-//   }
-//   var itemsNumber = document.getElementById("input2").getAttribute("value"); //.value;
-//   document.getElementById("somme").innerHTML =
-//     itemsNumber * parseInt(prix2, 10);
-// }
-
-// function susbstructItem(y) {
-//   y = document.getElementById(y);
-//   if (y.value > 0) {
-//     y.setAttribute("value", +y.value - 1);
-//     // y.value = +y.value - 1;
-//   }
-//   var itemsNumber = document.getElementById("input2").getAttribute("value"); //.value;
-//   document.getElementById("somme").innerHTML =
-//     itemsNumber * parseInt(prix2, 10);
-// }
 
 ///////////////////////////////Like//////////////////////////
 
@@ -62,8 +46,3 @@ let prix1 = document.getElementById("prix1").textContent;
 let prix2 = document.getElementById("prix2").textContent;
 let prix3 = document.getElementById("prix3").textContent;
 let somme = document.getElementById("somme");
-// function calulSomme() {
-// var itemsNumber = document.getElementById(x).getAttribute("value"); //.value;
-// document.getElementById("somme").innerHTML = itemsNumber * parseInt(prix1, 10);
-// }
-console.log(document.getElementById("somme").innerText);
